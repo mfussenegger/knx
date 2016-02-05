4a211587c6e54ccbd5ff692421764d6f88325416
Mathias Fussenegger <f.mathias@zignar.net>
2016-02-06 00:55:49 +0100

    add some more tests

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-from unittest import TestCase, main
+import unittest
+from unittest.mock import MagicMock, patch
 from doctest import DocTestSuite
 import knx
 
@@ -14,7 +14,7 @@ def f(output):
         output.append(x)
 
 
-class KnxTest(TestCase):
+class KnxTest(unittest.TestCase):
     def test_encode_ga(self):
         x = knx.encode_ga('0/1/14')
         self.assertEqual(270, x)
@@ -79,6 +79,30 @@ class KnxTest(TestCase):
         self.assertEqual(decoded.dst, '0/0/7')
         self.assertEqual(decoded.value, b'\x03\xA3\x03')
 
+    def test_call_write_with_str_addr(self):
+        writer = MagicMock()
+        knx.write(writer, '0/0/20', 1)
+        writer.write.assert_called_with(b"\x00\x06\x00'\x00\x14\x00\x81")
+
+    def test_call_write_with_ga_addr(self):
+        writer = MagicMock()
+        knx.write(writer, knx.GroupAddress(0, 0, 20), '1')
+        writer.write.assert_called_with(b"\x00\x06\x00'\x00\x14\x00\x81")
+
+    def test_call_read(self):
+        writer = MagicMock()
+        knx.read(writer, knx.GroupAddress(0, 0, 20))
+        writer.write.assert_called_with(b"\x00\x06\x00'\x00\x14\x00\x00")
+
+    @patch('socket.socket')
+    def test_connect_connects_to_socket(self, socket):
+        sock = MagicMock()
+        socket.return_value = sock
+        with knx.connect('localhost'):
+            sock.connect.assert_called_once_with(('localhost', 6720))
+            sock.send.assert_called_once_with(b'\x00\x05\x00&\x00\x00\x00')
+        sock.close.assert_called_with()
+
 
 def load_tests(loader, tests, ignore):
     tests.addTests(DocTestSuite(knx))
@@ -86,4 +110,4 @@ def load_tests(loader, tests, ignore):
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
